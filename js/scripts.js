02d56e635e53faf6112e5350ac098b4fdc065680
Kimiko Muwanguzi <kimikomu@gmail.com>
2017-02-23 11:29:11 -0500

    Code in Window.matchMedia() function loads automatically when the screen is resized and on load. Fixes #3

--- js/scripts.js
+++ js/scripts.js
@@ -48,8 +48,6 @@ $("#gallery li").click(function(event){
   $captionBody.text(captionBodyText);
 
   $('body').addClass('freeze');
-  
-
 });
 
 // hide overlay when clicked
@@ -61,7 +59,6 @@ $overlay.click(function(){
 
   $('body').removeClass('freeze');
 });
-
 
 
 
@@ -91,73 +88,86 @@ $galleryItem.mouseover(function(){
 
 
 
-/* ========== Load based on media query ========== 
-   Carousel source: https://github.com/codepo8/simple-carousel/blob/master/carousel-simplest.html */
+/* ========== Load Content Based on Screen Size ========== */
+   
 
-//  on window resize - eventlistener for window size?????
+var loadContentAccordingToSize = function() {
+  // the viewport is less than 768 pixels wide
+  if (window.matchMedia("(max-width: 768px)").matches) {
 
-// the viewport is at least 768 pixels wide
-if (window.matchMedia("(max-width: 768px)").matches) {
-
-  // load carousel in Services Section
-  carousel = (function(){
-    // Read necessary elements from the DOM once
-    var box = document.querySelector('.carouselbox');
-    var next = box.querySelector('.next');
-    var prev = box.querySelector('.prev');
-    
-    // Define the global counter, the items and the current item 
-    var counter = 0;
-    var items = box.querySelectorAll('.col');
-    var amount = items.length;
-    var current = items[0];
-
-    box.classList.add('active');
-
-    // navigate through the carousel
-    function navigate(direction) {
-      // hide the old current list item 
-      current.classList.remove('current');
+    // load carousel in Services Section
+    // Carousel source: https://github.com/codepo8/simple-carousel/blob/master/carousel-simplest.html
+    carousel = (function(){
+      // Read necessary elements from the DOM once
+      var box = document.querySelector('.carouselbox');
+      var next = box.querySelector('.next');
+      var prev = box.querySelector('.prev');
       
-      // calculate th new position
-      counter = counter + direction;
-      // if the previous one was chosen and the counter is less than 0 make the counter the last element,
-      // thus looping the carousel
-      if (direction === -1 && 
-          counter < 0) { 
-        counter = amount - 1; 
+      // Define the global counter, the items and the current item 
+      var counter = 0;
+      var items = box.querySelectorAll('.col');
+      var amount = items.length;
+      var current = items[0];
+
+      box.classList.add('active');
+
+      // navigate through the carousel
+      function navigate(direction) {
+        // hide the old current list item 
+        current.classList.remove('current');
+        
+        // calculate th new position
+        counter = counter + direction;
+        // if the previous one was chosen and the counter is less than 0 make the counter the last element,
+        // thus looping the carousel
+        if (direction === -1 && 
+            counter < 0) { 
+          counter = amount - 1; 
+        }
+
+        // if the next button was clicked and there is no items element, set the counter to 0
+        if (direction === 1 && 
+            !items[counter]) { 
+          counter = 0;
+        }
+
+        // set new current element and add CSS class
+        current = items[counter];
+        current.classList.add('current');
       }
 
-      // if the next button was clicked and there is no items element, set the counter to 0
-      if (direction === 1 && 
-          !items[counter]) { 
-        counter = 0;
-      }
+      // add event handlers to buttons
+      next.addEventListener('click', function(ev) {
+        navigate(1);
+      });
+      prev.addEventListener('click', function(ev) {
+        navigate(-1);
+      });
+      
+      // show the first element (when direction is 0 counter doesn't change)
+      navigate(0);
+    })(); 
 
-      // set new current element and add CSS class
-      current = items[counter];
-      current.classList.add('current');
-    }
+    // $(".flex-grid").find('div').removeAttr('data-tilt').removeAttr('style');
 
-    // add event handlers to buttons
-    next.addEventListener('click', function(ev) {
-      navigate(1);
-    });
-    prev.addEventListener('click', function(ev) {
-      navigate(-1);
-    });
-    
-    // show the first element (when direction is 0 counter doesn't change)
-    navigate(0);
-  })();
+  };
 
-// the viewport is larger than 768 pixels wide
-} else {
-    // add data-tilt to divs in flex-grid in Services Section
-  $(".flex-grid").find('div').attr('data-tilt', '');
-}
+  // else if (window.matchMedia("(min-width: 769px)").matches){
+  //     if (/* data-tilt is not already added */) {
+  //       // add data-tilt to divs in flex-grid in Services Section
+  //       $(".flex-grid").find('div').attr('data-tilt', '');
+  //       console.log("matchMedia is working");
+  //     };
+  // };
 
 
+};
+
+// load appropriate content on page load
+window.onload = loadContentAccordingToSize;
+
+// load appropriate content on screen size adjustment
+window.onresize = loadContentAccordingToSize;
 
 
 
